a5a845ed7374cc58d0cd803a555834309845af96
Huanliang Fan <fanhuanliang@gmail.com>
2021-06-18 20:50:22 -0700

    Setup webpack, server. Init setting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
 const path = require('path');
-const SRC_DIR = path.join(__dirname, '/src');
-const DIST_DIR = path.join(__dirname, '/public');
+const SRC_DIR = path.join(__dirname, './src');
+const DIST_DIR = path.join(__dirname, "./public");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  
-}
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: "main.js",
+    path: DIST_DIR,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
