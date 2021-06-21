ac52015d1d93d00623bde52cb0e3e24b9d72e193
Huanliang Fan <fanhuanliang@gmail.com>
2021-06-20 21:36:07 -0700

    Set up server, implement register api, use bcrypt to hash the password before sent to database.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,59 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const app = express();
-const port = 3000;
+const bcrypt = require('bcrypt');
+const port = process.env.port || 3000; //whatever is in the environment variable PORT, or 3000 if there's nothing there.
+const db = require('../database/index.js')
+const bodyParser = require("body-parser");
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.post('/api/login', (req, res)=>{
+  console.log(req.body)
+  db.loginUser(req.body, (err, result)=>{
+    if(err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  })
+})
+
+app.post('/api/register', async (req, res)=>{
+  // console.log("req1", req.body);
+  // db.registerUser(req.body, (err, result) => {
+  //   if (err) {
+  //     res.status(404).send(err);
+  //   } else {
+  //     // console.log(result);
+  //     res.status(200).send(result);
+  //   }
+  // });
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = req.body;
+    user.password = hashedPassword;
+    // console.log(user);
+    db.registerUser(user, (err, result) => {
+      if (err) {
+        res.status(404).send(err);
+      } else {
+        // console.log(result);
+        res.status(200).send(result);
+      }
+    });
+  } catch {
+    res.status(500).send(0)
+  }
+})
+
+
 
 app.listen(port, ()=> {
   console.log(`Listening at http://localhost:${port}`);
