3affb399d7d06cbbbc55291487e8189344a041f8
Huanliang Fan <fanhuanliang@gmail.com>
2021-06-30 11:55:15 -0700

    Create dummy data for testing front end.

---
+++ DummyData.js
@@ -0,0 +1,26 @@
+const dummyData = [
+  {
+    id: "1",
+    userName: "jacky",
+    email: "jacky@gmail.com",
+    phoneNumber: "408888888",
+    password: "123",
+    balance: 1000.0,
+  },
+  {
+    id: "2",
+    userName: "john",
+    email: "john@gmail.com",
+    phoneNumber: "408888881",
+    password: "123",
+    balance: 1000.0,
+  },
+  {
+    id: "3",
+    userName: "lucy",
+    email: "lucy@gmail.com",
+    phoneNumber: "408888882",
+    password: "123",
+    balance: 1000.0,
+  },
+];
