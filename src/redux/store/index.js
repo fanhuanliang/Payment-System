1d19f3aabcbaf06a6214cad3c2a6eecdd9083008
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-02 15:38:56 -0700

    Add redux tool extension to keep tracking the store state.

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,12 @@
 import {createStore} from 'redux'
 import reducer from '../reducer/reducer'
 
-const store = createStore(reducer)
-
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+//Redux DevTools with basic store. 
+//createStore(reducer, [preloadedState], [enhancer])
+//[preloadedState] (any): The initial state.
+//If this is a boolean, it will be used to indicate whether configureStore should automatically enable support for the Redux DevTools browser extension.
 export default store;
