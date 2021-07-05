6160142903a7784211f5d355774f8840152278fb
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-05 10:40:09 -0700

    Implement the register submit action and update the store state.

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -1,4 +1,5 @@
-import * as type from './actionTypes'
+import * as type from "./actionTypes";
+const axios = require("axios");
 
 export const handleInputValue = (name, value) => {
   return {
@@ -10,23 +11,36 @@ export const handleInputValue = (name, value) => {
 export const handleInitState = () => {
   return {
     type: type.CLEAN_UP_STATE,
-  };  
-}
-
+  };
+};
 
 export const registerSubmitHandler = (registerData) => (dispatch) => {
   // thunk allows us return function by passing dispatch
-  console.log(registerData);
-  const requestOptions = {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(registerData),
-  };
-  fetch("api/register", requestOptions)
-    .then((response) => response.json())
-    .then((data) =>
+  axios
+    .post("/api/register", registerData)
+    .then((response) => {
+      console.log("response", response);
+    })
+    .catch((error) => {
       dispatch({
-        type: type.REGISTER_SUBMIT_HANDLER,
-      })
-    );
+        type: type.REGISTER_ERROR_HANDLER,
+        payload: {
+          errMessage: "Some of your info isn't correct. Please try again.",
+        },
+      });
+      console.log("error", error);
+    });
 };
+// const requestOptions = {
+//   method: "POST",
+//   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//   body: JSON.stringify([registerData]),
+// };
+// fetch("api/register", requestOptions)
+//   .then((response) => response.json())
+//   .then((data) =>
+//   console.log(data),
+//     dispatch({
+//       type: type.REGISTER_SUBMIT_HANDLER,
+//     })
+//   );
--- src/redux/reducer/formReducer.js
+++ src/redux/reducer/formReducer.js
@@ -10,14 +10,17 @@ const initialState = {
   phoneNumber: "",
   regPassword: "",
   regConfirmPassword: "",
+  errMessage: ''
 };
 export default (state = initialState, action) => {
   // console.log(action.payload) 
   switch (action.type) {
     case type.HANDLE_INPUT_VALUE:
-      return {...state, ...action.payload}
+      return { ...state, ...action.payload };
     case type.CLEAN_UP_STATE:
-      return initialState;  
+      return initialState;
+    case type.REGISTER_ERROR_HANDLER:
+      return { ...state, ...action.payload};
     default:
       return state;
   }
