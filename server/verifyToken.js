67de339de6526f1a20115d895b5eb2161b10b1a4
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-05 23:27:51 -0700

    Fixed the bug.  Please enter the commit message for your changes. Lines starting

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -1,26 +1,23 @@
 const jwt = require("jsonwebtoken");
-
 // Verify Token
 function verifyToken(req, res, next) {
   //Get auth header value
   const bearerHeader = req.headers["authorization"];
   //check if bearer is undefined
-  if (typeof bearerHeader !== "undefined") {
-    //Authorization: Bearer <access_token>
-    const bearerToken = bearerHeader.split(" ")[1];
-    // console.log(bearerToken);
-    jwt.verify(bearerToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(403).json({ msg: "Token is not valid" });
-      }
-      req.user = user;
+    if (!bearerHeader)
+      return res.status(401).json({ msg: "No token, authorization denied" });
+
+    try {
+      // Verify token Authorization: Bearer <access_token>
+      const bearerToken = bearerHeader.split(" ")[1];
+      const decoded = jwt.verify(bearerToken, process.env.ACCESS_TOKEN_SECRET);
+      console.log('verify', decoded)
+      // Add user from payload
+      req.user = decoded;
       next();
-    });
-    //next middleware
-  } else {
-    //Forbidden
-    res.sendStatus(401).json({ msg: "No token, authorization denied" });
-  }
+    } catch (e) {
+      res.status(400).json({ msg: "Token is not valid" });
+    }
 }
 
 module.exports = verifyToken;
