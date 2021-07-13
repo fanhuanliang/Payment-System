425719fc843c20ed3fcb187048617119e22fdd1a
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-13 15:05:00 -0700

    Implement the HOC to the register component.

--- src/components/HOC/withAuth.js
+++ src/components/HOC/withAuth.js
@@ -1,65 +1,81 @@
-import React from 'react'
-import { Link, useHistory } from "react-router-dom";
+import React from "react";
+import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {
   handleInputValue,
   handleInitState,
   loginSubmitHandler,
+  registerSubmitHandler,
 } from "../../redux/actions/actionCreators";
 import { clearErrors } from "../../redux/actions/errorActions";
 
-const withAuth = ( OrgComponent ) => {
+const withAuth = (OrgComponent) => {
   const newComponent = () => {
     const formReducer = useSelector((state) => state.formReducer);
     const errorReducer = useSelector((state) => state.errorReducer);
     const authReducer = useSelector((state) => state.authReducer);
-    const {user,loginPassword,userName,email,regPassword,regConfirmPassword} = formReducer;
-    const { msg, id } = errorReducer;
+    const {
+      user,
+      loginPassword,
+      userName,
+      email,
+      regPassword,
+      regConfirmPassword,
+    } = formReducer;
     const { isAuthenticated } = authReducer;
 
-  const dispatch = useDispatch();
-  let history = useHistory();
-  const linkStyle = {
-    backgroundColor: "rgb(163, 205, 217)",
-    border: "0px",
-  };
-  const historyAuth = () => {
-    // history.push("/main");
-    if (isAuthenticated) history.push("/main");
-  };
+    const dispatch = useDispatch();
+    let history = useHistory();
+    const linkStyle = {
+      backgroundColor: "rgb(163, 205, 217)",
+      border: "0px",
+    };
+    const historyAuth = () => {
+      if (isAuthenticated) history.push("/main");
+    };
 
-  React.useEffect(() => {
-    return dispatch(handleInitState());
-  }, []);
-  // React.useEffect(() => registerAuth(), [isAuthenticated]);
-  React.useEffect(() => historyAuth(), [isAuthenticated]);
+    React.useEffect(() => {
+      return dispatch(handleInitState());
+    }, []);
 
-  const handleChange = (event) => {
-    dispatch(handleInputValue(event.target.name, event.target.value));
-  };
+    React.useEffect(() => historyAuth(), [isAuthenticated]);
 
-  const removeErrors = () => {
-    dispatch(clearErrors());
-  };
+    const handleChange = (event) => {
+      dispatch(handleInputValue(event.target.name, event.target.value));
+    };
 
-  const handleLoginSubmit = (event) => {
-    event.preventDefault();
-    const password = loginPassword;
-    const account = user;
-    dispatch(loginSubmitHandler({ account, password }));
-  };
+    const removeErrors = () => {
+      dispatch(clearErrors());
+    };
+
+    const handleLoginSubmit = (event) => {
+      event.preventDefault();
+      const password = loginPassword;
+      const account = user;
+      dispatch(loginSubmitHandler({ account, password }));
+    };
+
+    const handleRegisterSubmit = (event) => {
+      event.preventDefault();
+      const password = regPassword;
+      dispatch(
+        registerSubmitHandler({ userName, email, password, regConfirmPassword })
+      );
+    };
+
     return (
       <OrgComponent
         formReducer={(user, loginPassword)}
         errorReducer={errorReducer}
         handleChange={handleChange}
-        handleSubmit={handleLoginSubmit}
+        handleLoginSubmit={handleLoginSubmit}
         removeErrors={removeErrors}
         linkStyle={linkStyle}
+        handleRegisterSubmit={handleRegisterSubmit}
       />
     );
-  }
+  };
   return newComponent;
-}
+};
 
-export default withAuth
+export default withAuth;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,56 +1,12 @@
 import React from 'react';
 import * as style from './Register.style.jsx';
-import { Link, useHistory } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux';
-import { handleInputValue, handleInitState, registerSubmitHandler } from '../../redux/actions/actionCreators'
-import { clearErrors } from '../../redux/actions/errorActions'
+import { Link } from 'react-router-dom'
+import withAuth from '../HOC/withAuth'
 
-const Register = () => {
-  const { userName, email, regPassword, regConfirmPassword } = useSelector(state => state.formReducer)
-  const { msg, id } = useSelector(state => state.errorReducer)
-  const { isAuthenticated } = useSelector(state => state.authReducer)
-
-  // console.log('userName', msg, id)
-  let history = useHistory()
-
-  const registerAuth = () => {
-    if (isAuthenticated) history.push("/main")
-  }
-
-  React.useEffect(() =>
-    registerAuth(), [isAuthenticated]
-  )
-
-  const dispatch = useDispatch()
-  
-  React.useEffect(() => {
-    return dispatch(
-      handleInitState()
-    )
-  }, [])
-
-  const handleChange = (event) => {
-    dispatch(
-      handleInputValue(
-        event.target.name,
-        event.target.value
-      )
-    )
-  }
-
-  const removeErrors = () => {
-    dispatch(
-      clearErrors()
-    )
-  }
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const password = regPassword
-    dispatch(
-      registerSubmitHandler({ userName, email, password, regConfirmPassword })
-    )
-  }
-  
+const Register = (props) => {
+  const { userName, email, regPassword, regConfirmPassword } = props.formReducer
+  const { msg, id } = props.errorReducer
+  const { handleChange, removeErrors, handleRegisterSubmit } = props
   return (
     <style.Layer>
       <style.Container>
@@ -58,13 +14,12 @@ const Register = () => {
           <h1><Link to='/'>Sign up for Mimic Pay</Link></h1>
         </style.Header>
         {id === 'REGISTER_FAIL' ? <div onMouseLeave={removeErrors}>{msg.msg}</div> : <div style={{ visibility: 'hidden' }}>No err</div>}
-        <style.Form onSubmit={handleSubmit}>
+        <style.Form onSubmit={handleRegisterSubmit}>
           <input type="text" name='userName' placeholder="Username" value={userName} onChange={handleChange} />
           <input type="email" name='email' placeholder="Email" value={email} onChange={handleChange} />
           <input type="password" name='regPassword' placeholder="Password" value={regPassword} onChange={handleChange} />
           <input type="password" name='regConfirmPassword' placeholder="Confirm" value={regConfirmPassword} onChange={handleChange} />
           <style.Button >Register</style.Button>
-          {/* <style.LogInButton>Log In Instead</style.LogInButton> */}
           <Link to='login'><style.LogInButton>Log In Instead</style.LogInButton></Link>
         </style.Form>
       </style.Container>
@@ -73,4 +28,4 @@ const Register = () => {
 }
 
 
-export default Register;
+export default withAuth(Register);
