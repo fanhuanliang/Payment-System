5477080fff39a812f70c5fd2dbb4196fc72f8d38
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-14 14:57:25 -0700

    Create style varible file.

---
+++ src/styles/variable.style.jsx
@@ -0,0 +1,42 @@
+import { css } from 'styled-components';
+
+//font
+export default {
+  //font
+  font_family_monospace: `SFMono-Regular, Menlo, Monaco, Consolas,
+  'Liberation Mono', 'Courier New'`,
+  font_family_base: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+  'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+  'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
+  font_size_base: `0.8rem`,
+  font_weight_lighter: `lighter`,
+  font_weight_light: `300`,
+  font_weight_normal: `400`,
+  font_weight_bold: `700`,
+  font_weight_bolder: `bolder`,
+  font_weight_base: `400`,
+
+  // theme
+  white: `#fff`,
+  gray_100: `#f8f9fa`,
+  gray_200: `#e9ecef`,
+  gray_300: `#dee2e6`,
+  gray_400: `#ced4da`,
+  gray_500: `#adb5bd`,
+  gray_600: `#6c757d`,
+  gray_700: `#495057`,
+  gray_800: `#343a40`,
+  gray_900: `#212529`,
+  black: `#000`,
+  blue: `#0d6efd`,
+  indigo: `#6610f2`,
+  purple: `#6f42c1`,
+  pink: `#d63384`,
+  red: `#dc3545`,
+  orange: `#fd7e14`,
+  yellow: `#fadb14`,
+  dark_yellow: `#ffb533`,
+  green: `#52c41a`,
+  teal: `#20c997`,
+  cyan: `#17a2b8`,
+}
