127c05da2e08b39676609b02308d7e062ecb6113
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-13 21:55:12 -0700

    Update the  status to 409 if user exist. fixed the 500 status code didn't send.

--- server/authServer.js
+++ server/authServer.js
@@ -17,12 +17,13 @@ app.post("/api/login", async (req, res) => {
   // console.log("login server", req.body);
   const { account, password } = req.body;
   if (!account || !password) {
+    // 400 Bad Request response status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error
     return res.status(400).json({ msg: "Please enter all fields" });
   }
   try {
     db.loginUser(req.body, async (err, user) => {
       if (err) {
-        //check if user exist
+        //Can't find the user
         res.status(404).json({ msg: err });
       } else {
         //Validate password
@@ -34,9 +35,9 @@ app.post("/api/login", async (req, res) => {
             process.env.ACCESS_TOKEN_SECRET,
             (err, token) => {
               if (err) {
+                //The HTTP 403 Forbidden client error status response code indicates that the server understood the request but refuses to authorize it.
                 res.sendStatus(403);
               } else {
-                // console.log(user.balance);
                 res
                   .status(200)
                   .json({ token, user: { id: user.id, userName: user.userName, balance:user.balance} });
@@ -49,8 +50,8 @@ app.post("/api/login", async (req, res) => {
         }
       }
     });
-  } catch {
-    res.status(500).send();
+  } catch(err) {
+    res.status(500).send(err);
   }
 });
 
@@ -66,11 +67,12 @@ app.post("/api/register", async (req, res) => {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
       const user = req.body;
       user.password = hashedPassword;
-      const balance = { balance: 1000.0 }; // default balance 1000
-      // console.log('user', user)
-      db.registerUser({ ...user, ...balance }, (err, user) => {
+      const initBalance = { balance: 1000.0 }; // default balance 1000
+
+      db.registerUser({ ...user, ...initBalance }, (err, user) => {
         if (err) {
-          res.status(404).json({ msg: err });
+          //check if user exist ref:https://stackoverflow.com/questions/9269040/which-http-response-code-for-this-email-is-already-registered/9270432
+          res.status(409).json({ msg: err });
         } else {
           const { id, userName, balance } = user;
           const token = jwt.sign({ userName }, process.env.ACCESS_TOKEN_SECRET);
@@ -80,8 +82,8 @@ app.post("/api/register", async (req, res) => {
           });
         }
       });
-    } catch {
-      res.status(500);
+    } catch(err) {
+      res.status(500).json(err);
     }
 });
 
