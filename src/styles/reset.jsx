98318cdc46f4aaede6c39546337bc6503d56fc01
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-14 15:36:20 -0700

    Reset the css, using meyerweb, and add some custom setting:  font-family, font-weight, and line-height to the reset body.

--- src/styles/reset.jsx
+++ src/styles/reset.jsx
@@ -1,44 +1,95 @@
 import { css } from 'styled-components';
 import variable from './variable.style.jsx';
 
-console.log(variable)
+// console.log(variable)
 export const reset = css`
-// style lint-disable at-rule-no-vendor-prefix, declaration-no-important, selector-no-qualifying-type, property-no-vendor-prefix
-
-// CSS reset
-//
-// Normalization of HTML elements, manually forked from Normalize.css to remove
-// styles targeting irrelevant browsers while applying new styles.
-//
-// Normalize is licensed MIT. https://github.com/necolas/normalize.css
-
-// Document
-//
-
-// Change from 'box-sizing: content - box' so that 'width' is not affected by 'padding' or 'border'.
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
-
-// Body
-//
-// 1. Remove the margin in all browsers.
-// 2. As a best practice, apply a default "background - color".
-// 3. Prevent adjustments of font size after orientation changes in IE on Windows Phone and in iOS.
-// 4. Change the default tap highlight to be completely transparent in iOS.
-body {
-  margin: 0; // 1
-  padding: 0;
-  font-family: ${variable.font_family_base};
-  font-size: ${variable.font_size_base};
-  font-weight: ${variable.font_weight_base};
-  line-height: $line-height-base;
-  color: ${variable.gray_900};
-  text-align: $body-text-align;
-  background-color: $body-bg; // 2
-  -webkit-text-size-adjust: 100%; // 3
-  -webkit-tap-highlight-color: rgba($black, 0); // 4
-}
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  main, menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    font-family: ${variable.font_family_base};
+    font-weight: ${variable.font_weight_base};
+    line-height: ${variable.line_height_base};
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, main, menu, nav, section {
+    display: block;
+  }
+  /* HTML5 hidden-attribute fix for newer browsers */
+  *[hidden] {
+    display: none;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  /* http://www.paulirish.com/2012/box-sizing-border-box-ftw/ (2015/04/28)*/
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  /* Additional resets */
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  button {
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: auto;
+    overflow: visible;
+    background: transparent;
+    color: inherit;
+    font: inherit;
+    text-align: inherit;
+    outline: none;
+    line-height: inherit;
+    -webkit-appearance: none;
+  }
+  /* Fix antialiasing */
+  *, *:before, *:after {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  /* Disable user select on everything but texts */
+  *, *:before, *:after {
+    user-select: none;
+  }
+  p, h1, h2, h3, h4, h5, h6, blockquote, pre, ul, ol, li, table, tr, th, td, input, textarea {
+    user-select: text;
+  }
 `
+
