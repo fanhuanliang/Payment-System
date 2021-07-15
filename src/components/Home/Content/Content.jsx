18acb21b117c76adc739c6b32eae5115add26493
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-15 14:40:14 -0700

    Fixed the formatting.

--- src/components/Home/Content/Content.jsx
+++ src/components/Home/Content/Content.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import * as style from './Content.style.jsx'
-import img from '../../../../source/money.jpeg'//local import jpeg file by installing file-loader in webpack.
+import React from "react";
+import * as style from "./Content.style.jsx";
+import img from "../../../../source/money.jpeg"; // local import jpeg file by installing file-loader in webpack.
 
 export default function Content() {
   return (
@@ -8,16 +8,16 @@ export default function Content() {
       <style.Wrapper>
         <style.Container>
           <style.LeftSide>
-          <style.H1>Online payments</style.H1>
+            <style.H1>Online payments</style.H1>
             <style.UnOrderList>
               <li>Mimic the transfer feature of venmo</li>
               <li>Money can be transferred to other user</li>
               <li>Maximum $1000 in one transaction</li>
             </style.UnOrderList>
-            </style.LeftSide>
-          <img src={img} alt='Money transfer' />
+          </style.LeftSide>
+          <img src={img} alt="Money transfer" />
         </style.Container>
       </style.Wrapper>
     </div>
-  )
+  );
 }
--- src/components/Home/Content/Content.style.jsx
+++ src/components/Home/Content/Content.style.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
-import variable from "../../../styles/variable.style.jsx"
-import { Heading } from "../../../styles/Heading.style.jsx"
+import variable from "../../../styles/variable.style.jsx";
+import { Heading } from "../../../styles/Heading.style.jsx";
 
 export const Wrapper = styled.div`
   background-color: ${variable.teal};
   min-height: 80vh;
   margin: 10px;
-`
+`;
 
 export const Container = styled.div`
-  display:flex;
+  display: flex;
   justify-content: space-evenly;
-  > h1, li {
+  > h1,
+  li {
     padding: 20px;
     margin: 20px;
     color: #557e94;
@@ -19,18 +20,20 @@ export const Container = styled.div`
   @media only screen and (max-width: 1000px) {
     justify-content: center;
     flex-direction: column;
-    };
-`
+  } ;
+`;
+
 export const LeftSide = styled.div`
   font-size: 2em;
-  margin:auto;
-`
+  margin: auto;
+`;
+
 export const UnOrderList = styled.ul`
   list-style-type: none;
-`
+`;
 
 export const H1 = styled(Heading)`
   font-size: 2.2rem;
   padding: 15px;
   margin: 15px;
-`
+`;
