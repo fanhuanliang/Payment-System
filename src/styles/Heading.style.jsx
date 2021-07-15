cb2775d9c4b7a7196e075803bb651bef38f26466
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-14 21:25:21 -0700

    change the font-size to 50px

--- src/styles/Heading.style.jsx
+++ src/styles/Heading.style.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const H1 = styled.h1`
   font-family: 'Raleway',sans-serif; 
-  font-size: 62px; 
+  font-size: 50px; 
   font-weight: 800;
   text-transform: uppercase
 }
