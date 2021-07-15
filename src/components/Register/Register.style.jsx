2277eb8d4dcc3ad8b082b7cfcdb020dbf7fd2147
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-14 21:27:42 -0700

    Import the heading initbutton and variable. Delete the loginbutton style.

--- src/components/Register/Register.style.jsx
+++ src/components/Register/Register.style.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import * as init from '../../index.style.jsx';
+import { InitButton } from '../../styles/button.style.jsx';
+import { Heading } from '../../styles/Heading.style.jsx'
+import variable from '../../styles/variable.style.jsx';
 
 export const Layer = styled.div`
     display: flex;
@@ -10,20 +12,23 @@ export const Layer = styled.div`
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    font-size: 1.5em;
     align-items: center;
     margin: auto;
     width: 50vw;
-    border: 1px solid ${init.color.border_gray};
+    border: 1px solid ${variable.border_gray};
     border-radius: 10px;
+    &> div {
+      font-size: 1.5rem;
+    }
 `
 
 export const Header = styled.header`
-    font-family: emoji;
-    font-size: 1.2em;
-    padding: 10px;
-    margin: 10px; 
+    padding: 20px;
+    margin-top: 20px; 
 `
+
+export const H1 = styled(Heading)``
+
 export const Form = styled.form`
     display: flex;
     flex-direction: column;
@@ -31,47 +36,17 @@ export const Form = styled.form`
     padding: 20px;
     & > input, button {
       padding: 10px;
-      margin: 10px;
-      font-size: 20px;
-      border: 1px solid ${init.color.border_gray};
+      margin: 18px;
+      font-size: 1.7rem;
+      border: 1px solid ${variable.border_gray};
       border-radius: 5px;
       width: 95%;
     }
+    &> button {
+      background-color:${variable.dark_blue}
+    }
 `
-export const Button = styled.button`
-	background-color:${init.color.dark_blue};
-	border-radius:5px;
-	border:1px solid #4e6096;
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:20px;
-	font-weight:bold;
-	text-decoration:none;
-`
-
-export const LogInButton = styled.button`
-	box-shadow:inset 0px 1px 3px 0px #d9fbbe;
-	background:linear-gradient(to bottom, #b8e356 5%, #a5cc52 100%);
-	background-color:#b8e356;
-	border-radius:5px;
-	border:1px solid #83c41a;
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:20px;
-	font-weight:bold;
-	padding:11px 23px;
-	text-decoration:none;
-  width: 95%;
-  :hover {
-	background:linear-gradient(to bottom, #a5cc52 5%, #b8e356 100%);
-	background-color:#a5cc52;
-  }
-  :active {
-	position:relative;
-	top:1px;
-}
+export const Button = styled(InitButton)`
+	background-color:${variable.green};
+  text-align: center
 `
