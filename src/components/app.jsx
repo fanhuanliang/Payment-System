719aaea6e96989cc4198af189634ddb05fc45f01
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-15 10:27:45 -0700

    Add loading feature.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,16 +5,19 @@ import Register from './Register/Register.jsx';
 import Main from './Main/Main.jsx';
 import { GlobalStyle } from '../styles/index.style.jsx';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { useDispatch } from 'react-redux';
+import { useDispatch ,useSelector } from 'react-redux';
 import { loadUser } from '../redux/actions/actionCreators'
-
+import { Loading } from '../styles/Loading.style.jsx'
 const App = () => {
   // console.log(window.__REDUX_DEVTOOLS_EXTENSION__() )
-
+  const { isLoading } = useSelector(state=>state.authReducer)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(loadUser())
   }, [])
+
+  if(isLoading) return <Loading>Loading...</Loading>
+  
   return (
     <Router>
       <>
