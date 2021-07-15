d9826dd95c89a5b8a423b71afe9450a2c19cbfc3
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-15 14:31:07 -0700

    Fixed the format.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,44 +1,45 @@
-import React, { useEffect } from 'react';
-import Login from './Login/Login.jsx';
-import Home from './Home/Home.jsx';
-import Register from './Register/Register.jsx';
-import Main from './Main/Main.jsx';
-import { GlobalStyle } from '../styles/index.style.jsx';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { useDispatch ,useSelector } from 'react-redux';
-import { loadUser } from '../redux/actions/actionCreators'
-import { Loading } from '../styles/Loading.style.jsx'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Login from "./Login/Login.jsx";
+import Home from "./Home/Home.jsx";
+import Register from "./Register/Register.jsx";
+import Main from "./Main/Main.jsx";
+import { GlobalStyle } from "../styles/index.style.jsx";
+import { loadUser } from "../redux/actions/actionCreators";
+import { Loading } from "../styles/Loading.style.jsx";
+
 const App = () => {
   // console.log(window.__REDUX_DEVTOOLS_EXTENSION__() )
-  const { isLoading } = useSelector(state=>state.authReducer)
-  const dispatch = useDispatch()
+  const { isLoading } = useSelector((state) => state.authReducer);
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(loadUser())
-  }, [])
+    dispatch(loadUser());
+  }, []);
 
-  if(isLoading) return <Loading>Loading...</Loading>
-  
+  if (isLoading) return <Loading>Loading...</Loading>;
+
   return (
     <Router>
       <>
-      <GlobalStyle />
+        <GlobalStyle />
         <Switch>
-          <Route exact path='/'>
-            <Home /> 
+          <Route exact path="/">
+            <Home />
           </Route>
-          <Route exact path='/login'>
+          <Route exact path="/login">
             <Login />
           </Route>
-          <Route exact path='/register'>
+          <Route exact path="/register">
             <Register />
           </Route>
-          <Route exact path='/main'>
+          <Route exact path="/main">
             <Main />
           </Route>
         </Switch>
       </>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
