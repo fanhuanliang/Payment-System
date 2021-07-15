68070bc4c726558f08234d9d4a39c56a004e94e0
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-14 22:36:48 -0700

    Delete color object.

--- src/styles/index.style.jsx
+++ src/styles/index.style.jsx
@@ -2,13 +2,8 @@
 import { createGlobalStyle } from 'styled-components'
 import { reset } from './reset.jsx'
 
-const color = {
-  light_blue: 'rgb(163, 205, 217)',
-  dark_blue: 'rgb(0,108,182)',
-  border_gray: 'rgb(203, 207, 209)',
-}
 // console.log(body)
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   // *,
   // *::before,
   // *::after {
@@ -23,7 +18,3 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
 `
 
-export {
-  color,
-  GlobalStyle,
-}
