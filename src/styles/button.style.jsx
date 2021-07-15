87649bebd50ecd537b68c251b376d3ec239f52a5
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-14 20:31:53 -0700

    Build a reusable button.

---
+++ src/styles/button.style.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+	background-color:#b8e356;
+	border-radius:5px;
+	border:1px solid ;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:20px;
+	font-weight:bold;
+	padding:11px 23px;
+	text-decoration:none;
+  width: 95%;
+  :active {
+	position:relative;
+	top:1px;
+}
+`;
+
+export const InitButton = ({ children, className }) => {
+  // console.log(className) child className
+  return <Button className={className}>{children}</Button>;
+};
