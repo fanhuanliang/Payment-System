a8422761ff8bafa6e031537f37297ccfd34fa570
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-15 10:27:30 -0700

    Build a loading style componet handle when the isLoading

---
+++ src/styles/Loading.style.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled, { keyframes }from 'styled-components';
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.svg`
+  animation: ${rotate360} 1s linear infinite;
+  transform: translateZ(0);
+
+  border-top: 2px solid grey;
+  border-right: 2px solid grey;
+  border-bottom: 2px solid grey;
+  border-left: 4px solid black;
+  background: transparent;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+const Loader = styled.div`
+  position: fixed;
+  top: 50%;
+  left:50%;
+  transform: translate(-50%, -50%);
+`
+
+export const Loading = () => {
+  return <Loader><Spinner></Spinner></Loader>
+};
