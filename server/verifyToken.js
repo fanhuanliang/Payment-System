66d52fc802f9cb853bb29f1cd7d40406cd2a5e34
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-15 21:00:02 -0700

    Change to verify cookies token.

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -1,15 +1,18 @@
 const jwt = require("jsonwebtoken");
+
 // Verify Token
 function verifyToken(req, res, next) {
+  const accessToken = req.cookies["access-token"];
+  // console.log("accessToken", accessToken);
   // Get auth header value
-  const token = req.headers.authorization;
+  // const token = req.headers.authorization;
   // check if bearer is undefined, no token
-  if (!token) {
+  if (!accessToken) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
     // Add user from payload
     req.user = decoded;
     next();
