88cc5dc33f42ce72771b79a97f4f7a51fb8a1b23
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-15 20:57:22 -0700

    Success store the httponly cookies.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const cookieParser = require("cookie-parser");
 
 const app = express();
 // const bcrypt = require("bcrypt");
@@ -13,7 +16,7 @@ const db = require("../database/index.js");
 const verifyToken = require("./verifyToken");
 
 app.use(express.static(path.join(__dirname, "..", "public")));
-
+app.use(cookieParser());
 app.use(cors());
 // support parsing of application/json type post data
 // app.use(bodyParser.json());
@@ -81,6 +84,90 @@ app.put("/api/transfer", verifyToken, (req, res) => {
       }
     }
   );
+});
+
+app.post("/api/login", async (req, res) => {
+  // console.log("login server", req.body);
+  const { account, password } = req.body;
+  if (!account || !password) {
+    // 400 Bad Request response status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+  try {
+    db.loginUser(req.body, async (err, user) => {
+      if (err) {
+        // Can't find the user
+        res.status(404).json({ msg: err });
+      } else {
+        // Validate password
+        if (await bcrypt.compare(req.body.password, user.password)) {
+          // jwt.sign(payload, secretOrPrivateKey, [options, callback])
+          const { userName } = user;
+          jwt.sign(
+            { userName },
+            process.env.ACCESS_TOKEN_SECRET,
+            // { expiresIn: "15s" },
+            (error, token) => {
+              if (error) {
+                // The HTTP 403 Forbidden client error status response code indicates that the server understood the request but refuses to authorize it.
+                res.sendStatus(403);
+              } else {
+                res.cookie("access-token", token, {
+                  httpOnly: true,
+                });
+                res.status(200).json({
+                  token,
+                  user: {
+                    id: user.id,
+                    userName: user.userName,
+                    balance: user.balance,
+                  },
+                });
+              }
+            }
+          );
+        } else {
+          // console.log("passwordWrong");
+          res.status(400).json({ msg: "Invalid credentials" });
+        }
+      }
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post("/api/register", async (req, res) => {
+  // console.log(req.body);
+  const { userName, email, password, regConfirmPassword } = req.body;
+  if (!userName || !email || !password || !regConfirmPassword) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+  if (password !== regConfirmPassword) {
+    return res.status(401).json({ msg: "Password does not match" });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = req.body;
+    user.password = hashedPassword;
+    const initBalance = { balance: 1000.0 }; // default balance 1000
+
+    db.registerUser({ ...user, ...initBalance }, (err, resultUser) => {
+      if (err) {
+        // check if user exist ref:https://stackoverflow.com/questions/9269040/which-http-response-code-for-this-email-is-already-registered/9270432
+        res.status(409).json({ msg: err });
+      } else {
+        const { id, userName, balance } = resultUser;
+        const token = jwt.sign({ userName }, process.env.ACCESS_TOKEN_SECRET);
+        res.status(200).json({
+          token,
+          user: { id, userName, balance },
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // wildcard handles any requests that don't match the ones ABOVE
