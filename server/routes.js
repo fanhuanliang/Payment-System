07f2496d09f3f51e3a4898ef343e2155277145f6
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-16 22:42:48 -0700

    Add 3  more api endpoint.

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const { loginHandler, registerHandler } = require("./controllers/auth");
 const { verifyToken } = require("./middleware/verifyToken");
-const { loadedUser } = require("./controllers/users");
+const {
+  loadedUser,
+  searchRecipient,
+  transactionHandler,
+} = require("./controllers/users");
+const { deleteCookie } = require("./middleware/handleCookies");
 
 const routes = (app) => {
   // Login endpoint
@@ -11,6 +17,21 @@ const routes = (app) => {
 
   // Retrieve login user info
   app.get("/api/auth/user", verifyToken, loadedUser);
+
+  // Search recipient user
+  app.post("/api/findRecipient", verifyToken, searchRecipient);
+
+  // Handle the transaction
+  app.put("/api/transfer", verifyToken, transactionHandler);
+
+  // Handle logout
+  app.get("/api/deleteCookie", deleteCookie);
+
+  // wildcard handles any requests that don't match the ones ABOVE
+  app.get("*", (req, res) => {
+    // console.log('here', res)
+    res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
+  });
 };
 
 module.exports = routes;
