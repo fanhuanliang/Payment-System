79c006f614ff0c3d1fdd04cc661b940bd688f49c
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-16 21:50:32 -0700

    Add auth/user endpoint.

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,16 @@
-// const { request, respond } = require("express");
-const { loginHandler } = require("./controllers/auth.js");
+const { loginHandler, registerHandler } = require("./controllers/auth");
 const { verifyToken } = require("./middleware/verifyToken");
+const { loadedUser } = require("./controllers/users");
 
 const routes = (app) => {
   // Login endpoint
   app.post("/api/login", loginHandler);
-  // console.log(verifyToken);
-  // app.get("/api/auth/user", verifyToken);
+
+  // Register endpoint
+  app.post("/api/register", registerHandler);
+
+  // Retrieve login user info
+  app.get("/api/auth/user", verifyToken, loadedUser);
 };
 
 module.exports = routes;
