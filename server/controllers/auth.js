168d7df46895fbdfcf4f0dd8033c9fc884fdce3b
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-16 21:50:54 -0700

    Fixed the formatting.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 
 const bcrypt = require("bcrypt");
-const { User } = require("../../database/index.js");
+const { User } = require("../../database/index");
 const {
   accessTokenGenerator,
   refreshTokenGenerator,
 } = require("../middleware/generateToken");
 const { handleError } = require("../middleware/handleError");
-const { sendCookie } = require("../middleware/sendCookie");
+const { sendCookie } = require("../middleware/handleCookies");
 
 const loginHandler = async (req, res) => {
   const { account, password } = req.body;
@@ -43,6 +43,43 @@ const loginHandler = async (req, res) => {
   }
 };
 
+const registerHandler = async (req, res) => {
+  // console.log(req.body);
+  const { userName, email, password, regConfirmPassword } = req.body;
+  if (!userName || !email || !password || !regConfirmPassword) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+  if (password !== regConfirmPassword) {
+    return res.status(401).json({ msg: "Password does not match" });
+  }
+  try {
+    const userInfo = {
+      $or: [{ userName: req.body.userName }, { email: req.body.email }],
+    };
+    // Check if user exists
+    const findUser = await User.findOne(userInfo);
+    if (findUser) throw "user exists";
+    // create hashed password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = req.body;
+    user.password = hashedPassword;
+    user.balance = 1000; // default balance 1000
+    const newUser = new User(user);
+    // Store the new User.
+    const saveUser = await newUser.save();
+    if (!saveUser) throw "Something went wrong saving the user";
+    const accessToken = accessTokenGenerator(user.userName);
+    const refreshToken = refreshTokenGenerator(user.userName);
+
+    if (!accessToken) throw "Could not sign the token";
+    return sendCookie(res, accessToken, refreshToken, newUser);
+  } catch (error) {
+    // console.log(error);
+    handleError(res, error);
+  }
+};
+
 module.exports = {
   loginHandler,
+  registerHandler,
 };
