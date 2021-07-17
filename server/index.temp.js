b69d36f2ec853f9535477ef9f3b2766a3ecaa9da
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-16 21:50:08 -0700

    Add cookie-parse.

--- server/index.temp.js
+++ server/index.temp.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 
 const port = process.env.port || 3000;
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 // support parsing of application/json type post data
 // app.use(bodyParser.json());
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
