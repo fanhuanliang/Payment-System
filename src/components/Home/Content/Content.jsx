b53515ab113c00a67b391290ab8d4b45e8e50a20
Huanliang Fan <fanhuanliang@gmail.com>
2021-07-18 19:28:05 -0700

    Change the path of image.

--- src/components/Home/Content/Content.jsx
+++ src/components/Home/Content/Content.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as style from "./Content.style.jsx";
-import img from "../../../../source/money.jpeg"; // local import jpeg file by installing file-loader in webpack.
+import img from "../../images/money.jpeg"; // local import jpeg file by installing file-loader in webpack.
 
 export default function Content() {
   return (
@@ -10,9 +10,9 @@ export default function Content() {
           <style.LeftSide>
             <style.H1>Online payments</style.H1>
             <style.UnOrderList>
-              <li>Mimic the transfer feature of venmo</li>
+              <li>Mimic the fund transfer feature of Venmo</li>
               <li>Money can be transferred to other user</li>
-              <li>Maximum $1000 in one transaction</li>
+              <li>You will get $1000 when you register new account</li>
             </style.UnOrderList>
           </style.LeftSide>
           <img src={img} alt="Money transfer" />
